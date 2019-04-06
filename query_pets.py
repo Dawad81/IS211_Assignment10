1f10078f3242c3b0c62b5544141ec34df78be157
Dawad81 <dennis.awad83@spsmail.cuny.edu>
2019-04-06 14:16:40 -0400

    query_pets.py working in full, need to edit and run pylint

--- query_pets.py
+++ query_pets.py
@@ -14,20 +14,25 @@ with con:
     while True:
         query_id = raw_input("\nPlease enter a persons ID number, to view "
                              "them and their pets information. \n \n"
-                             "Enter [-1] to EXIT the program\n")
-        cur.execute("SELECT first_name, last_name, person.age, name, breed, "
-                    "pet.age, dead FROM person, pet, person_pet "
-                    "WHERE person_id = person.id AND"
-                    " pet_id = pet.id AND person_pet.person_id = (?)", (query_id))
+                             "Enter [-1] to EXIT the program\n\n")
+        if query_id == '-1':
+            print 'Program exiting.......\nGood bye!'
+            raise SystemExit
+        else:
+            cur.execute("SELECT first_name, last_name, person.age, name, breed, "
+                        "pet.age, dead FROM person, pet, person_pet "
+                        "WHERE person_id = person.id AND"
+                        " pet_id = pet.id AND person_pet.person_id = (?)", query_id)
+            rows = cur.fetchall()
+            if len(rows) == 0:
+                print '*' * 50
+                print 'No person in the data base with an ID number of {}.\nPlease try a different ID number.'.format(query_id)
+                print '*' * 50
+            for row in rows:
+                print '{} {}, {} years old'.format(row[0], row[1], row[2])
+                print '-' * 30
 
-        rows = cur.fetchall()
-
-        print rows
-
-        for row in rows:
-            print '{} {}, {} years old'.format(row[0], row[1], row[2])
-            if row[6] == 1:
-                #print '{} {}, {} years old'.format(row[0], row[1], row[2])
-                print '{} {} currently owns {}, a {}, that is {} years old.'.format(row[0],row[1],row[3],row[4],row[5])
-            else:
-                print '{} {} owned {}, a {}, that passed away at the age of {}.'.format(row[0],row[1],row[3],row[4],row[5])
+                if row[6] == 0:
+                    print '{} {} currently owns {}, a {}, that is {} years old.\n'.format(row[0],row[1],row[3],row[4],row[5])
+                else:
+                    print '{} {} owned {}, a {}, that passed away at the age of {}.\n'.format(row[0],row[1],row[3],row[4],row[5])
