b51d8a313a66344586c89336308ceb56425fa541
Dawad81 <dennis.awad83@spsmail.cuny.edu>
2019-04-06 19:33:36 -0400

    Docstrings added, pylint edits

--- load_pets.py
+++ load_pets.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Docstring."""
+"""This module creates the pets.db SQL data base by creating tables with the
+provided list (person, pet, person_pet)."""
+
 
 import sqlite3 as lite
+
 
 person = ((1, 'James', 'Smith', 41),
           (2, 'Diana', 'Greene', 23),
@@ -10,9 +13,9 @@ person = ((1, 'James', 'Smith', 41),
           (4, 'William', 'Gibson', 23))
 
 pet = ((1, 'Rusty', 'Dalmation', 4, 1),
-       (2, 'Bella', 'AlaskanMalamute', 3, 0),
+       (2, 'Bella', 'Alaskan Malamute', 3, 0),
        (3, 'Max', 'Cocker Spaniel', 1, 0),
-       (4, 'Rocky','Beagle', 7, 0),
+       (4, 'Rocky', 'Beagle', 7, 0),
        (5, 'Rufus', 'Cocker Spaniel', 1, 0),
        (6, 'Spot', 'Bloodhound', 2, 1))
 
@@ -23,28 +26,18 @@ person_pet = ((1, 1),
               (3, 5),
               (4, 6))
 
-con = lite.connect('pets.db')
+CON = lite.connect('pets.db')
 
-#with con:
-#    cur = con.cursor()
-#    cur.execute("DROP TABLE IF EXISTS person")
-#    cur.execute("CREATE TABLE person(id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, age INTEGER)")
-#    cur.execute("INSERT INTO person (first_name, last_name, age) VALUES ('James','Smith',41)")
-#    cur.execute("INSERT INTO person (first_name, last_name, age) VALUES ('Diana','Greene',23)")
-#    cur.execute("INSERT INTO person (first_name, last_name, age) VALUES ('Sara','White',27)")
-#    cur.execute("INSERT INTO person (first_name, last_name, age) VALUES ('William','Gibson',23)")
-
-with con:
-    cur = con.cursor()
+with CON:
+    cur = CON.cursor()
     cur.execute("DROP TABLE IF EXISTS person")
-    cur.execute("CREATE TABLE person (id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, age INTEGER)")
+    cur.execute("CREATE TABLE person (id INTEGER PRIMARY KEY, "
+                "first_name TEXT, last_name TEXT, age INTEGER)")
     cur.executemany("INSERT INTO person VALUES(?, ?, ?, ?)", person)
     cur.execute("DROP TABLE IF EXISTS pet")
-    cur.execute("CREATE TABLE pet (id INTEGER PRIMARY KEY, name TEXT, breed TEXT, age INTEGER, dead INTEGER)")
+    cur.execute("CREATE TABLE pet (id INTEGER PRIMARY KEY, name TEXT, "
+                "breed TEXT, age INTEGER, dead INTEGER)")
     cur.executemany("INSERT INTO pet VALUES(?, ?, ?, ?, ?)", pet)
     cur.execute("DROP TABLE IF EXISTS person_pet")
     cur.execute("CREATE TABLE person_pet (person_id INTEGER, pet_id INTEGER)")
     cur.executemany("INSERT INTO person_pet VALUES(?, ?)", person_pet)
-
-
-
--- query_pets.py
+++ query_pets.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Docstring."""
+"""This module parses the pets.db SQL data base, to find a particular persons
+info along with there pets information, according to the entered persons
+ID number."""
 
 
 import sqlite3 as lite
@@ -12,27 +14,33 @@ with con:
     cur = con.cursor()
 
     while True:
-        query_id = raw_input("\nPlease enter a persons ID number, to view "
-                             "them and their pets information. \n \n"
-                             "Enter [-1] to EXIT the program\n\n")
+        query_id = raw_input('\nPlease enter a persons ID number, to view '
+                             'them and their pets information. \n \n'
+                             'Enter [-1] to EXIT the program\n\n')
         if query_id == '-1':
             print 'Program exiting.......\nGood bye!'
             raise SystemExit
         else:
-            cur.execute("SELECT first_name, last_name, person.age, name, breed, "
-                        "pet.age, dead FROM person, pet, person_pet "
+            cur.execute("SELECT first_name, last_name, person.age, name, "
+                        "breed, pet.age, dead FROM person, pet, person_pet "
                         "WHERE person_id = person.id AND"
-                        " pet_id = pet.id AND person_pet.person_id = (?)", query_id)
+                        " pet_id = pet.id AND person_pet.person_id = (?)",
+                        query_id)
             rows = cur.fetchall()
-            if len(rows) == 0:
+            if not rows:
                 print '*' * 50
-                print 'No person in the data base with an ID number of {}.\nPlease try a different ID number.'.format(query_id)
+                print 'No person in the data base with an ID number of {}.\n' \
+                      'Please try a different ID number.'.format(query_id)
                 print '*' * 50
             for row in rows:
                 print '{} {}, {} years old'.format(row[0], row[1], row[2])
                 print '-' * 30
 
                 if row[6] == 0:
-                    print '{} {} currently owns {}, a {}, that is {} years old.\n'.format(row[0],row[1],row[3],row[4],row[5])
+                    print '{} {} currently owns {}, a {}, that is {} ' \
+                          'years old.\n'.format(row[0], row[1], row[3],
+                                                row[4], row[5])
                 else:
-                    print '{} {} owned {}, a {}, that passed away at the age of {}.\n'.format(row[0],row[1],row[3],row[4],row[5])
+                    print '{} {} owned {}, a {}, that passed away at the ' \
+                          'age of {}.\n'.format(row[0], row[1], row[3],
+                                                row[4], row[5])
