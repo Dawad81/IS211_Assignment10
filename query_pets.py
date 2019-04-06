7b11dc8064b5fccc1c3872e30eeb2d0721b0e304
Dawad81 <dennis.awad83@spsmail.cuny.edu>
2019-04-06 13:45:03 -0400

    print statments return owner and pet data

--- query_pets.py
+++ query_pets.py
@@ -15,14 +15,19 @@ with con:
         query_id = raw_input("\nPlease enter a persons ID number, to view "
                              "them and their pets information. \n \n"
                              "Enter [-1] to EXIT the program\n")
-        
-        cur.execute("SELECT * FROM person WHERE person.id = (?)", (query_id)) #, pet, person_pet")
-    #rows = cur.fetchall()
-    #while True:
+        cur.execute("SELECT first_name, last_name, person.age, name, breed, "
+                    "pet.age, dead FROM person, pet, person_pet "
+                    "WHERE person_id = person.id AND"
+                    " pet_id = pet.id AND person_pet.person_id = (?)", (query_id))
 
         rows = cur.fetchall()
 
-    #if row == None:
-        #break
+        print rows
+
         for row in rows:
-            print row
+            print '{} {}, {} years old'.format(row[0], row[1], row[2])
+            if row[6] == 1:
+                #print '{} {}, {} years old'.format(row[0], row[1], row[2])
+                print '{} {} currently owns {}, a {}, that is {} years old.'.format(row[0],row[1],row[3],row[4],row[5])
+            else:
+                print '{} {} owned {}, a {}, that passed away at the age of {}.'.format(row[0],row[1],row[3],row[4],row[5])
