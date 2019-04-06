1ca54c972e76281df24710197f6f5d5cabca1b56
Dawad81 <dennis.awad83@spsmail.cuny.edu>
2019-04-06 11:33:47 -0400

    person prints

--- query_pets.py
+++ query_pets.py
@@ -12,8 +12,17 @@ with con:
     cur = con.cursor()
 
     while True:
-        query_id = raw_input('\nPlease enter a persons ID number, to view '
-                             'them and their pets information. \n \n'
-                             'Enter [-1] to EXIT the program\n')
-
+        query_id = raw_input("\nPlease enter a persons ID number, to view "
+                             "them and their pets information. \n \n"
+                             "Enter [-1] to EXIT the program\n")
         
+        cur.execute("SELECT * FROM person WHERE person.id = (?)", (query_id)) #, pet, person_pet")
+    #rows = cur.fetchall()
+    #while True:
+
+        rows = cur.fetchall()
+
+    #if row == None:
+        #break
+        for row in rows:
+            print row
